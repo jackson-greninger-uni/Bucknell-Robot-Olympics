76a0bd99cba9ce06bcbc4fa4176c7aee962a1013
Ben Concepcion <bjc032@bucknell.edu>
2025-11-14 14:23:13 -0500

    Robot curling stop after line working

--- robot/robot_curlingv2.py
+++ robot/robot_curlingv2.py
@@ -5,7 +5,7 @@ from machine import Pin
 
 # ok so the goal is to create a queue of the previous 5 offsets
 
-queue = [1,1,1,1,1]     # init with all ones
+queue = [1,1,1,1,1,1,1,1]     # init with all ones
 
 line_robot = LineFollowerRobot(20, kp=0.35, kd=0.01)
 
@@ -14,7 +14,9 @@ while abs(sum(queue)) > 0.001:
     queue.pop(0)
     queue.append(line_robot.last_offset)
     time.sleep_ms(1)
+    print(queue)
+    print(abs(sum(queue)))
 
-line_robot.go_straight()
-time.sleep( <amount of time till we hit target> )
+line_robot.go_straight(20)
+time.sleep(1)
 line_robot.stop()
