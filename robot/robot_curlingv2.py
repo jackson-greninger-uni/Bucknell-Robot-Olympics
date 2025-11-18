836d5ab0dd68cb584941c85db9be23e36df29cd5
JacksonGreninger <jwg019@bucknell.edu>
2025-11-18 09:11:18 -0500

    qualified for curling

--- robot/robot_curlingv2.py
+++ robot/robot_curlingv2.py
@@ -2,21 +2,25 @@ from util_line_follow import LineFollowerRobot
 from util_drive import Robot
 import time
 from machine import Pin
+import neopixel
 
 # ok so the goal is to create a queue of the previous 5 offsets
 
-queue = [1,1,1,1,1,1,1,1]     # init with all ones
+queue = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]     # init with all ones
 
 line_robot = LineFollowerRobot(20, kp=0.35, kd=0.01)
+pixels = neopixel.NeoPixel(machine.Pin(18), 2)
 
 while abs(sum(queue)) > 0.001:
-    line_robot.follow_line()
+    line_robot.follow_line("stop")
     queue.pop(0)
     queue.append(line_robot.last_offset)
     time.sleep_ms(1)
-    print(queue)
-    print(abs(sum(queue)))
 
+print("go straight")
 line_robot.go_straight(20)
-time.sleep(1)
+time.sleep(2)
+pixels.fill((0,0,255))
+pixels.write()
+time.sleep(0.7)
 line_robot.stop()
